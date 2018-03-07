b971de5d3c4c6675cf4f412cea3b1d10d587b70f
Seif Lotfy <seif.lotfy@gmail.com>
2018-03-07 17:42:36 +0000

    Remove ObserverSlow since this can be immitated with the payload and callback

--- hindsight.go
+++ hindsight.go
@@ -50,17 +50,10 @@ func (hs *Hindsight) push(elapsed time.Duration, done CallbackFunc, payload inte
 	}
 }
 
-// Observe returns a function that when called measures the elapsed duration
-// and triggeres the done with the payload as an argument
-// if called again it will return error
-func (hs *Hindsight) Observe(done CallbackFunc, payload interface{}) func() error {
-	return hs.ObserveSlow(0, done, payload)
-}
-
 // ObserveSlow returns a function that when called measures the elapsed duration
 // and triggeres the done with the payload as an argument if the given slow duration is exceeded
 // if called again it will return error
-func (hs *Hindsight) ObserveSlow(slow time.Duration, done CallbackFunc, payload interface{}) func() error {
+func (hs *Hindsight) Observe(done CallbackFunc, payload interface{}) func() error {
 	now := time.Now()
 	finished := false
 	return func() error {
@@ -68,9 +61,7 @@ func (hs *Hindsight) ObserveSlow(slow time.Duration, done CallbackFunc, payload 
 			return fmt.Errorf("observation over, can't call same func twice")
 		}
 		elapsed := time.Since(now)
-		if slow <= elapsed {
-			hs.push(elapsed, done, payload)
-		}
+		hs.push(elapsed, done, payload)
 		finished = true
 		return nil
 	}
